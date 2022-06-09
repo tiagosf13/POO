f2e053459dd3ba4ea8f28c6ca1bc556a536c9b79
tiagosf13 <t.fonseca@ua.pt>
2022-06-09 12:15:46 +0100

    AutoCommitVs

--- src/Semana11/Companhias.java
+++ src/Semana11/Companhias.java
@@ -4,17 +4,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Companhias {
     
-    public HashMap<String, ArrayList<String>> carregar_companhias(String ficheiro) throws IOException{
+    public ArrayList<String[]> carregar_companhias(String ficheiro) throws IOException{
         String word;
         String[] linha;
         ArrayList<String[]> lista_companhias = new ArrayList<>();
-        HashMap<String, ArrayList<String>> dic = new HashMap<>();
 
         Scanner input = new Scanner(new FileReader(ficheiro, Charset.forName("utf-8")));
         
@@ -26,16 +23,7 @@ public class Companhias {
             lista_companhias.add(linha);
         }
         lista_companhias.remove(0);
-        
-        for (String[] sub_list : lista_companhias){
-            if (sub_list.length == 3){
-                dic.put(sub_list[0], new ArrayList<>(Arrays.asList(sub_list[1], sub_list[2])));
-            }
-            else{
-                dic.put(sub_list[0], new ArrayList<>(Arrays.asList(sub_list[1])));
-            }
-        }
-        return dic;
+        return lista_companhias;
     }
 
 }
